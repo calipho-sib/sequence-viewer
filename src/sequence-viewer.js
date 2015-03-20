415b2eef21730ba7285031d895b374de0ff79f55
Mathieu Schaeffer <the.arthek@gmail.com>
2015-03-20 15:15:53 +0100

    Updated highlight and coverage

--- src/sequence-viewer.js
+++ src/sequence-viewer.js
@@ -1,66 +1,212 @@
 
-    /////////////
- 
-    function Sequence(sequence) {
-        var sequence = sequence;
- 
-        function renderHtml(divId){
- 
- 
-            var sources = "<div><h4>Protein Sequence ({{sequenceLength}})</h4></div>" +
-                "<div style=\"margin-top: 5px;\">" +
-                "<div id=\"charNumbers\" style=\"font-family: monospace;font-size: 12px;display:inline-block;text-align:right; padding-right:5px; border-right:1px solid LightGray;\"></div>" +
-                "<div id=\"fastaSeq\" display-option=\"30\" style=\"font-family: monospace;font-size: 12px;display:inline-block;padding:5px;\">{{{sequence}}}</div>" +
-                "<div><div style=\"display:inline-block;background:#C50063;width:20px;height:20px;vertical-align:middle;margin:0px 5px 0px 10px;\" class=\"img-circle\"></div><p style=\"display:inline-block;font-weight:bold;font-size:11px;font-style:italic;margin:0;padding-top:3px;vertical-align:top;\"> natural</p>" +
-                "<div style=\"display:inline-block;background:#F83919;width:20px;height:20px;vertical-align:middle;margin:0px 5px 0px 10px;\" class=\"img-circle\"></div><p style=\"display:inline-block;font-weight:bold;font-size:11px;font-style:italic;margin:0;padding-top:3px;vertical-align:top;\"> synthetic</p>" +
-                "<div style=\"display:inline-block;background:#69CC33;width:20px;height:20px;vertical-align:middle;margin:0px 5px 0px 10px;\" class=\"img-circle\"></div><p style=\"display:inline-block;font-weight:bold;font-size:11px;font-style:italic;margin:0;padding-top:3px;vertical-align:top;\"> proteotypic</p></div>" +
-                "</div>";
- 
-            var template = Handlebars.compile(sources);
-            var html = template({
-                "sequence": sequence,
-                "sequenceLength": sequence.length
-            });
-            $(divId).append(html);
- 
+
+/////////////
+
+function Sequence(sequence) {
+    var sequence = sequence;
+    var seqInit = "";
+    var lineJump = 0;
+
+    function renderHtml(divId, options){
+
+        if(typeof options.charsPerLine === 'undefined') {
+            lineJump=30;
+        }
+        else lineJump = options.charsPerLine;
+
+        var sources = "<div><h4>Protein Sequence ({{sequenceLength}})</h4></div>" +
+            "<div style=\"margin-top: 5px;\">" +
+            "<div id=\"charNumbers\" style=\"font-family: monospace;font-size: 12px;display:inline-block;text-align:right; padding-right:5px; border-right:1px solid LightGray;\"></div>" +
+            "<div id=\"fastaSeq\" display-option=\"" + lineJump + "\" style=\"font-family: monospace;font-size: 12px;display:inline-block;padding:5px;\">{{{sequence}}}</div>" +
+            "<div><div style=\"display:inline-block;background:#C50063;width:20px;height:20px;vertical-align:middle;margin:0px 5px 0px 10px;border-radius:50%;\"></div><p style=\"display:inline-block;font-weight:bold;font-size:11px;font-style:italic;margin:0;padding-top:3px;vertical-align:top;\">single</p>" +
+            "<div style=\"display:inline-block;background:#007800;width:20px;height:20px;vertical-align:middle;margin:0px 5px 0px 10px;border-radius:50%;\"></div><p style=\"display:inline-block;font-weight:bold;font-size:11px;font-style:italic;margin:0;padding-top:3px;vertical-align:top;\">multiple</p>" +
+            "<div style=\"display:inline-block;background:#69CC33;width:20px;height:20px;vertical-align:middle;margin:0px 5px 0px 10px;border-radius:50%;\"></div><p style=\"display:inline-block;font-weight:bold;font-size:11px;font-style:italic;margin:0;padding-top:3px;vertical-align:top;\">proteotypic</p></div>" +
+            "</div>";
+
+        var template = Handlebars.compile(sources);
+        var html = template({
+            "sequence": sequence,
+            "sequenceLength": sequence.length
+        });
+        $(divId).append(html);
+
+        if(!(options.wrapAminoAcids === false))
             sequenceLayout("#fastaSeq");
-            charNumberTwo("#fastaSeq","#charNumbers");
- 
- 
-        }
- 
-        var charNumberTwo = function(textAreaID,lineNumberID){
-            console.log(textAreaID);
-            var textContent = $(textAreaID).html().split("<br>");
-            var NBC = parseInt($(textAreaID).attr("display-option"));
-            console.log(NBC);
-            var newTextContent = [];
-            var charPerLine = 0;
-            console.log(textContent);
-            for (var i=0; i < textContent.length;i++){
-                newTextContent.push((charPerLine + 1) + ("<br>"));
-                charPerLine += NBC;
-            }
-            console.log(newTextContent);
-            $(lineNumberID).html(newTextContent.join(""));
- 
-        };
- 
-        var sequenceLayout = function(textAreaID){
- 
-            var newLines = parseInt($(textAreaID).attr("display-option"));
-            newLines = (newLines+(newLines/10)).toString();
-            var seqFormat = $(textAreaID).html();
-            seqFormat = seqFormat.toString().match(/.{1,10}/g).join(' ').match(new RegExp('.{1,'+newLines+'}','g')).join('<br>');
- 
-            $(textAreaID).html(seqFormat);
-        };
- 
- 
-        return {
-            render:renderHtml
-        }
+
+        if(!(options.showLineNumbers === false))
+            lineNumbers("#fastaSeq","#charNumbers");
+
+        seqInit = $("#fastaSeq").html();
     }
- 
- 
- 
+
+    function highlighting (start,end, color, options){
+        var positions = [start,end];
+        var hlSeq = seqInit;
+        positions[0]=positions[0]+~~(positions[0]/10)+4*(~~(positions[0]/lineJump));
+        positions[1]=positions[1]+~~(positions[1]/10)+4*(~~(positions[1]/lineJump));
+        var highlightColor=color;
+        hlSeq = hlSeq.substring(0,positions[0])+
+        "<span style=\"background:" + color + ";color:white;\">" +
+        hlSeq.substring(positions[0],positions[1]) +
+        "</span>" +
+        hlSeq.substring(positions[1],hlSeq.length);
+        $("#fastaSeq").html(hlSeq);
+    }
+
+    function coverage(list,start, end){
+        if (!start) var start=0;
+        if (!end) var end=0;
+        var HashAA = [];
+        for (var i=0;i<sequence.length;i++) {
+            var properties = [];
+            for (var j=0;j<list.length;j++) {
+                if (i >= list[j].position.first && i <= list[j].position.second) {
+                    properties.push(list[j].properties.join(""));
+                }
+            }
+            var clr = "black";
+            var underscore = false;
+            if (properties.length > 0) {
+                properties=properties.join("")
+                if (properties.match("synthetic")) underscore = true;
+                if (properties.match("natural")) {
+                    clr = "#007800";
+                }
+                if (properties.match(/natural/gi).length === 1) {
+                    clr = "#C50063";
+                }
+                if (properties.match("proteotypic")) {
+                    clr = "#00C500";
+                }
+            }
+            HashAA.push({"id": i, "property": properties, "color": clr, "underscore": underscore});
+        }
+
+        var HLon = false;
+        var HLcut = false;
+        var initStart=0;
+        var init="";
+        var initUnder="";
+        var source = "<span>";
+        var q=0;
+        for (var i=0;i<HashAA.length;i++){
+            j=i+~~(i/10)+4*(~~(i/lineJump));
+            if (i === end && i!==0) {
+                source+= seqInit.substring(initStart, j) +"</span>";
+                HLon=false;
+                initStart=j;
+            }
+            if (HLon === true && i !== (HashAA.length -1)){
+                if (HashAA[i+1].color !== init || HashAA[i+1].underscore !== initUnder) {
+                    source+=seqInit.substring(initStart, j) +"</span>";
+                    HLcut=true;
+                    initStart=j;
+                }
+            }
+            if (i === (HashAA.length -1)) {
+                if (initUnder === true && HLon === true) {
+                    source += seqInit.substring(initStart, seqInit.length) + "</span></span></span>";
+                }
+                else if (initUnder === true) {
+                    source += seqInit.substring(initStart, seqInit.length) + "</span></span>";
+                }
+                else {
+                    source += seqInit.substring(initStart, seqInit.length) + "</span>";
+                }
+            }
+            else if (HashAA[i+1].color !== init && HashAA[i+1].underscore == initUnder) {
+                init=HashAA[i+1].color;
+                if (initUnder === true) {
+                    source += seqInit.substring(initStart, j) + "</span></span><span style=\"color:" + HashAA[i + 1].color + ";\"><span style=\"text-decoration:underline;\">";
+                }
+                else {
+                    source += seqInit.substring(initStart, j) + "</span><span style=\"color:" + HashAA[i + 1].color + ";\">";
+                }
+                initStart=j;
+            }
+            else if (HashAA[i+1].color !== init && HashAA[i+1].underscore !== initUnder) {
+                init=HashAA[i+1].color;
+                initUnder=HashAA[i+1].underscore;
+                if (initUnder === true) {
+                    source += seqInit.substring(initStart, j) + "</span><span style=\"color:" + HashAA[i + 1].color + ";\"><span style=\"text-decoration:underline;\">";
+                }
+                else{
+                    source += seqInit.substring(initStart, j) + "</span></span><span style=\"color:" + HashAA[i + 1].color + ";\">";
+                }
+                initStart=j;
+            }
+            else if (HashAA[i+1].color == init && HashAA[i+1].underscore !== initUnder) {
+                init=HashAA[i+1].color;
+                initUnder=HashAA[i+1].underscore;
+                if (initUnder === true) {
+                    source += seqInit.substring(initStart,j) + "<span style=\"text-decoration:underline;\">";
+                }
+                else{
+                    source += seqInit.substring(initStart,j) + "</span>";
+                }
+                initStart=j;
+            }
+            if (HLcut===true) {
+                source+= "<span style=\"background:#FFE5A3;\">";
+                HLcut=false;
+            }
+            if (i+1 === start && i!==end) {
+                source+= seqInit.substring(initStart, j) +"<span style=\"background:#FFE5A3;\">";
+                HLon=true;
+                initStart = j;
+            }
+        }
+        $("#fastaSeq").html(source);
+
+
+
+
+        //var color = "";
+        //var property = element.parent().next().text();
+        //if (property.match("synthetic")) color="#F83919";
+        //else if (property.match("proteotypic")) color="#69CC33";
+        //else color="#C50063";
+        //console.log(color);
+        //return color;
+    }
+
+    var lineNumbers = function(textAreaID,lineNumberID){
+        var textContent = $(textAreaID).html().split("<br>");
+        var NBC = parseInt($(textAreaID).attr("display-option"));
+        var newTextContent = [];
+        var charPerLine = 0;
+        for (var i=0; i < textContent.length;i++){
+            newTextContent.push((charPerLine + 1) + ("<br>"));
+            charPerLine += NBC;
+        }
+        $(lineNumberID).html(newTextContent.join(""));
+
+    };
+
+    var sequenceLayout = function(textAreaID){
+
+        var newLines = parseInt($(textAreaID).attr("display-option"));
+        newLines = (newLines+(newLines/10)).toString();
+        var seqFormat = $(textAreaID).html();
+        seqFormat = seqFormat.toString().match(/.{1,10}/g).join(' ').match(new RegExp('.{1,'+newLines+'}','g')).join('<br>');
+
+        $(textAreaID).html(seqFormat);
+    };
+
+
+    return {
+        render:renderHtml,
+        selection:highlighting,
+        coverage:coverage
+    }
+}
+
+
+
+
+
+
+
+
+
+
